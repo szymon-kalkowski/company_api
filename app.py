22a3aff631bdfc8797db236c2087b5c2bee7b188
Szymon Kalkowski <szymon.kalkowski@wp.pl>
2023-01-14 15:33:45 +0100

    PUT, DELETE, subordinates

--- app.py
+++ app.py
@@ -15,16 +15,25 @@ driver = GraphDatabase.driver(uri, auth=(user, password), database="neo4j")
 
 def get_employees(tx):
     args = request.args.to_dict()
-    query = f"MATCH (e:Employee) RETURN e"
+    query = f"""MATCH (e:Employee) 
+                RETURN e
+                """
     if 'sort' in args.keys() and 'search' in args.keys():
-        query = f"MATCH (e:Employee) WHERE e.name CONTAINS '{args['search']}' OR e.position CONTAINS '{args['search']}' RETURN e"
+        query = f"""MATCH (e:Employee) 
+                    WHERE e.name CONTAINS '{args['search']}' 
+                    OR e.position CONTAINS '{args['search']}' 
+                    RETURN e
+                    """
         if args['sort'] in ['position', 'name']:
             query += f" ORDER BY e.{args['sort']}"
     elif 'sort' in args.keys():
         if args['sort'] in ['position', 'name']:
             query += f" ORDER BY e.{args['sort']}"
     elif 'search' in args.keys():
-        query = f"MATCH (e:Employee) WHERE e.name CONTAINS '{args['search']}' OR e.position CONTAINS '{args['search']}' RETURN e"
+        query = f"""MATCH (e:Employee) 
+                    WHERE e.name CONTAINS '{args['search']}' 
+                    OR e.position CONTAINS '{args['search']}' 
+                    RETURN e"""
     results = tx.run(query).data()
     employees = [{'name': result['e']['name'],
                   'position': result['e']['position']} for result in results]
@@ -37,20 +46,84 @@ def add_employee(tx):
     department = request.json['department']
     relation = request.json['relation']
 
-    res = tx.run(f"MATCH (e:Employee{{ name: '{name}' }}) RETURN e;").data()
+    res = tx.run(f"""MATCH (e:Employee{{ name: '{name}' }}) 
+                     RETURN e""").data()
     if len(res) == 0:
-        query = f"MATCH (d:Department{{ name: '{department}' }}) CREATE (e:Employee{{ name: '{name}', position: '{position}'}}) -[:{relation}]-> (d) RETURN e;"
+        query = f"""MATCH (d:Department{{ name: '{department}' }}) 
+                    CREATE (e:Employee{{ name: '{name}', position: '{position}'}}) -[:{relation}]-> (d) 
+                    RETURN e"""
         tx.run(query)
 
 
 def update_employee(tx, id):
-    query = f"MATCH (e:Employee) WHERE e.id={id}"
+    query = f"""MATCH (e:Employee) 
+                WHERE id(e)={id}
+                RETURN e;"""
+    result = tx.run(query).data()
+
+    name = request.json['name']
+    position = request.json['position']
+    department = request.json['department']
+    relation = request.json['relation']
+
+    query = f"""MATCH (e:Employee) 
+                WHERE id(e)={id}
+                """
+
+    if department and relation:
+        query = f"""MATCH (nd:Department{{name: '{department}'}})
+                    MATCH (e:Employee) -[r]-> (d:Department) 
+                    WHERE id(e)={id}
+                    DELETE r
+                    CREATE (e) -[:{relation}]-> (nd)
+                    """
+    if name:
+        query += f"""SET e.name = '{name}'
+                     """
+    if position:
+        query += f"""SET e.position = '{position}'
+                     """
+
+    query += "RETURN e;"
+
+    if not result:
+        return None
+    else:
+        result = tx.run(query).data()
+        return result
+
+
+def delete_employee(tx, id):
+    query = f"""MATCH (e:Employee) 
+                WHERE id(e) = {id} 
+                RETURN e"""
     result = tx.run(query).data()
 
     if not result:
         return None
     else:
-        query = ""
+        query = f"""MATCH (e:Employee) -[r]-> () 
+                    WHERE id(e) = {id}
+                    DELETE e, r"""
+        tx.run(query)
+        return {'status': 'success'}
+
+
+def get_subordinates(tx, id):
+    query = f"""MATCH (e:Employee) -[:WORKS_IN]-> (d:Department) <-[:MANAGES]- (m:Employee)
+                WHERE id(m) = {id}
+                RETURN e"""
+    results = tx.run(query).data()
+    employees = [{'name': result['e']['name'],
+                  'position': result['e']['position']} for result in results]
+    return employees
+
+
+def get_department_info(tx, id):
+    query = f"""MATCH (e:Employee) -[r]-> (d:Department) <-[:MANAGES]- (m:Employee)
+                WHERE id(e)={id}
+                MATCH (es:Employee) -[r]-> (d)
+                """
 
 
 @app.route('/employees', methods=['GET', 'POST'])
@@ -74,11 +147,36 @@ def put_delete_employee_route(id):
         with driver.session() as session:
             employee = session.execute_write(update_employee, id)
         if not employee:
-            response = {'message': 'Movie not found'}
+            response = {'message': 'Employee not found'}
             return jsonify(response), 404
         else:
             response = {'status': 'success'}
             return jsonify(response)
+    elif request.method == 'DELETE':
+        with driver.session() as session:
+            employee = session.execute_write(delete_employee, id)
+        if not employee:
+            response = {'message': 'Employee not found'}
+            return jsonify(response), 404
+        else:
+            response = {'status': 'success'}
+            return jsonify(response)
+
+
+@app.route('/employees/<id>/subordinates', methods=['GET'])
+def get_subordinates_route(id):
+    with driver.session() as session:
+        employees = session.execute_read(get_subordinates, id)
+    response = {'employees': employees}
+    return jsonify(response)
+
+
+@app.route('/employees/<id>/department', methods=['GET'])
+def get_department_info_route(id):
+    with driver.session() as session:
+        department = session.execute_read(get_department_info, id)
+    response = {'departmant': department}
+    return jsonify(response)
 
 
 if __name__ == '__main__':
